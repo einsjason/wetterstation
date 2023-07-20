efde39fe71fa24b949a011c9dbec62afff7f008b
Jason <139622632+einsjason@users.noreply.github.com>
2023-07-20 21:52:05 +0200

    update

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ async function getWeatherWarnings() {
 	const dom = document.getElementById("warnings");
 	dom.innerHTML = "";
 
-	let data = await getDataText("https://rammer.org/api/weather/weatherwarnings.json");
+	let data = await getDataText("https://rammer.org/_api/files/file.php?file=%2FTestdaten%2Fweatherwarnings.json");//getDataText("https://rammer.org/api/weather/weatherwarnings.json");
 
 	try {
 		data = JSON.parse(data);
